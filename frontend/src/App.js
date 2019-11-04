30019ebc8e6445c1dab73e1590a7ac631e2d9f38
Ben Potter <bpotte11@emich.edu>
2019-11-03 20:32:16 -0500

    auto update working

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,34 @@
 import React from 'react';
 import './App.css';
-import data from "./data.js"
 import dataL from "./dataL.js"
 import Checkbox from "./Checkbox.js"
 
+const API_BASE_URL = 'http://localhost:3001';
 
 class App extends React.Component {
-    componentWillMount = () => {
+    state = {leaderboardData: []}
+
+    componentDidMount = () => {
+        this.fetchLeaderboardData();
         this.selectedCheckboxes = new Set();
+        this.interval = setInterval(() => this.fetchLeaderboardData(), 2500);
+    };
+
+    componentWillUnmount() {
+        clearInterval(this.interval);
+    };
+
+    fetchLeaderboardData = () => {
+        fetch(`${API_BASE_URL}/leaderboard`)
+            .then(response => response.json())
+            .then((rows) => {
+                this.setState({
+                    leaderboardData: rows
+                })
+            })
+            .catch((error) => {
+
+            })
     };
 
     toggleCheckbox = label => {
@@ -37,18 +58,31 @@ class App extends React.Component {
         />)
     };
 
+    renderRows = (data) => {
+        return data.map(flight => <tr key={flight.id}>
+            <td> {flight.rank} </td>
+            <td> {flight.pilot.name} </td>
+            <td> {flight.duration_ms} </td>
+            <td> {flight.group} </td>
+            <td> {flight.pilot.org} </td>
+            <td> {flight.pilot.major} </td>
+            <td> {flight.id} </td>
+        </tr>);
+    };
+
     createCheckboxes = (items) => (
         items.map(this.createCheckbox)
     );
 
     render() {
-        const data1 = data.slice(0, 3);
-        const data2 = data.slice(3);
+        const data1 = this.state.leaderboardData.slice(0, 3);
+        const data2 = this.state.leaderboardData.slice(3);
         return (
             <div className="App">
                 <header className="App-header">
                     <h1>Rankings</h1>
                     <table className="App-table">
+                        <tbody>
                         <tr>
                             <th> Rank</th>
                             <th> Name</th>
@@ -58,15 +92,7 @@ class App extends React.Component {
                             <th> Major</th>
                             <th> Flight ID</th>
                         </tr>
-                        {data1.map(i => <tr>
-                            <td> {i.rank} </td>
-                            <td> {i.name} </td>
-                            <td> {i.fTime} </td>
-                            <td> {i.group} </td>
-                            <td> {i.org} </td>
-                            <td> {i.major} </td>
-                            <td> {i.flight_id} </td>
-                        </tr>)}
+                        {this.renderRows(data1)}
                         <tr className="App-ignore">
                             <td></td>
                             <td></td>
@@ -76,32 +102,25 @@ class App extends React.Component {
                             <td></td>
                             <td></td>
                         </tr>
-                        {data2.map(i => <tr>
-                            <td> {i.rank} </td>
-                            <td> {i.name} </td>
-                            <td> {i.fTime} </td>
-                            <td> {i.group} </td>
-                            <td> {i.org} </td>
-                            <td> {i.major} </td>
-                            <td> {i.flight_id} </td>
-                        </tr>)}
+                        {this.renderRows(data2)}
+                        </tbody>
                     </table>
                     <br></br>
                     <h3> Filters </h3>
-                    <div class="row">
-                        <div class="column">
+                    <div className="row">
+                        <div className="column">
                             <h4> Groups </h4>
                             <form>
                                 {this.createCheckboxes(dataL.groups)}
                             </form>
                         </div>
-                        <div class="column">
+                        <div className="column">
                             <h4> Majors </h4>
                             <form>
                                 {this.createCheckboxes(dataL.majors)}
                             </form>
                         </div>
-                        <div class="column">
+                        <div className="column">
                             <h4> Organizations </h4>
                             <form>
                                 {this.createCheckboxes(dataL.organizations)}
